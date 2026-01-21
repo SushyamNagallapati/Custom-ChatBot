9ec76c00e6a49728dd0f5a52c8c9bb860bd48207
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-21 12:28:34 -0500

    Input validation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -26,7 +27,22 @@ app.get('/api/hello', (req: Request, res: Response) => {
 //     conv2 -> 200
 const conversations = new Map<string, string>();
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim() // to get rid of white spaces at the beginning or end of the string
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long (max 1000 characters.'),
+   conversationId: z.string().uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+
    const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
